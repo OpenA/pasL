6fb7a4757b902c7ac3c97984e7105dd97a17a060
OpenA <adminother@gmail.com>
2022-01-14 18:55:16 +0500

    implement native event api and safe methods

--- pasL.js
+++ pasL.js
@@ -1,5 +1,5 @@
 
-class pasL {
+class PasL {
 
 	constructor(options = {}) {
 		/* "lock"    - is just lock aspect of selected area (selected area proportions). You can change this parameter later
@@ -29,106 +29,109 @@ class pasL {
 		zoneCenter.append( zoneTop , zoneSelect, zoneBottom );
 		// center horisontal block (selection block) has four absolute position corners
 		zoneSelect.append(
-			_setup('div', { id: 'rcon_t-l', class: 'pasl-rcons' }),
-			_setup('div', { id: 'rcon_t-r', class: 'pasl-rcons' }),
-			_setup('div', { id: 'rcon_b-l', class: 'pasl-rcons' }),
-			_setup('div', { id: 'rcon_b-r', class: 'pasl-rcons' })
+			_setup('div', { class: 'pasl-rcons t-l' }),
+			_setup('div', { class: 'pasl-rcons t-r' }),
+			_setup('div', { class: 'pasl-rcons b-l' }),
+			_setup('div', { class: 'pasl-rcons b-r' })
 		);
 		if (edgies) { // and optional for active edges
 			zoneSelect.append(
-				_setup('div', { id: 'rcon_l-c', class: 'pasl-rcons' }),
-				_setup('div', { id: 'rcon_t-c', class: 'pasl-rcons' }),
-				_setup('div', { id: 'rcon_r-c', class: 'pasl-rcons' }),
-				_setup('div', { id: 'rcon_b-c', class: 'pasl-rcons' })
+				_setup('div', { class: 'pasl-rcons l-c' }),
+				_setup('div', { class: 'pasl-rcons t-c' }),
+				_setup('div', { class: 'pasl-rcons r-c' }),
+				_setup('div', { class: 'pasl-rcons b-c' })
 			);
 		}
 
 		this.lock    = lock;
-		this.autoadd = autoadd;
-		this.box     = box;
-		this.coords  = Object.create(null);
-		this.zone    = {
-			top   : zoneTop   .style,
-			right : zoneRight .style,
-			left  : zoneLeft  .style,
-			bottom: zoneBottom.style,
-			select: zoneSelect.style,
-			center: zoneCenter.style
+		const coords = Object.create(null);
+
+		// this functions change width / height styles above the block groups
+		const setPosition = (x, y) => {
+			const { w, h, imgW, imgH } = coords;
+			if (Number.isFinite(x)) {
+				zoneLeft .style.width = `${coords.x1 = (x = imgW < x + w ? imgW - w : x > 0 ? x : 0) }px`;
+				zoneRight.style.width = `${coords.x2 = imgW - x - w }px`;
+			}
+			if (Number.isFinite(y)) {
+				zoneTop   .style.height = `${coords.y1 = (y = imgH < y + h ? imgH - h : y > 0 ? y : 0) }px`;
+				zoneBottom.style.height = `${coords.y2 = imgH - y - h }px`;
+			}
 		}
-		this.onMoveStart  = [];
-		this.onMoveChange = [];
-		this.onMoveEnd    = [];
+		const setWidth = (w, re = false) => {
+			const imgW = coords.imgW;
+			let x = re ? coords.x2 : coords.x1; 
+			if (Number.isFinite(w)) {
+				w = imgW < w + x ? imgW - x : w > 0 ? w : 0,
+				x = imgW - x - w;
+				if (re) {
+					zoneLeft.style.width = `${coords.x1 = x }px`;
+				} else {
+					zoneRight.style.width = `${coords.x2 = x }px`;
+				}
+				zoneSelect.style.width = `${coords.w = w }px`;
+			}
+		}
+		const setHeight = (h, re = false) => {
+			const imgH = coords.imgH;
+			let y = re ? coords.y2 : coords.y1; 
+			if (Number.isFinite(h)) {
+				h = imgH < h + y ? imgH - y : h > 0 ? h : 0,
+				y = imgH - y - h;
+				if (re) {
+					zoneTop.style.height = `${coords.y1 = y }px`;
+				} else {
+					zoneBottom.style.height = `${coords.y2 = y }px`;
+				}
+				zoneSelect.style.height = `${coords.h = h }px`;
+			}
+		}
+		// this is a general function for apply selection area on the image
+		const selectZone = (img, x = 0, y = 0, w = 50, h = 50, autoadd = false) => { // x, y, w, h  arguments is optional and set the start selection position
+			const imgW = img.width; // it just gets the real and actual width / height of the first argument passed and blindly bounces off of them.
+			const imgH = img.height; // you can call this function as much as you like for recalc area params
+			coords.realW = img.naturalWidth  || imgW; // if naturalWidth/naturalHeight data not contains in element
+			coords.realH = img.naturalHeight || imgH; // the width/height are counted as them
+
+			box.style.width  = `${coords.imgW = imgW }px`;
+			box.style.height = `${coords.imgH = imgH }px`;
+
+			//first set the X position of select zone and its width
+			zoneSelect.style.width = `${coords.w  = imgW < w + x ? imgW - x : w }px`;
+			zoneLeft  .style.width = `${coords.x1 = imgW < x + w ? imgW - w : x }px`;
+			zoneRight .style.width = `${coords.x2 = imgW - x - w }px`;
+
+			//second set the Y position of select zone and its height
+			zoneSelect.style.height = `${coords.h  = imgH < h + y ? imgH - y : h }px`;
+			zoneTop   .style.height = `${coords.y1 = imgH < y + h ? imgH - h : y }px`;
+			zoneBottom.style.height = `${coords.y2 = imgH - y - h }px`;
+
+			if (autoadd) { // note: just set this flag to false if you first argument passing is not an element
+				img.before(box); // or you can just doing something like:
+			} // { width: 500, ..., before: (box) => my_place.append(box) }
+		}
+		Object.defineProperties(this, {
+			/* public */
+			box         : { enumerable: true, value: box },
+			selectZone  : { enumerable: true, value: selectZone  },
+			setPosition : { enumerable: true, value: setPosition },
+			setWidth    : { enumerable: true, value: setWidth    },
+			setHeight   : { enumerable: true, value: setHeight   },
+
+			/* private */
+			_coords     : { value: coords },
+		});
 	}
-	// this setters change width / height styles above the block groups
-	get x ( ) { return this.coords.x1 }
-	set x (i) {
-		const { w, imgW }     =    this.coords;
-		this.zone.left .width = `${this.coords.x1 = (i = imgW < i + w ? imgW - w : Math.max(i,0)) }px`;
-		this.zone.right.width = `${this.coords.x2 = imgW - i - w }px`;
-	}
-	get y ( ) { return this.coords.y1 }
-	set y (i) {
-		const { h, imgH }       =    this.coords;
-		this.zone.top   .height = `${this.coords.y1 = (i = imgH < i + h ? imgH - h : Math.max(i,0)) }px`;
-		this.zone.bottom.height = `${this.coords.y2 = imgH - i - h }px`;
-	}
-	get w ( ) { return this.coords.w }
-	set w (i) {
-		const { x1, imgW }     =    this.coords;
-		this.zone.select.width = `${this.coords.w  = (i = imgW < i + x1 ? imgW - x1 : Math.max(i,0)) }px`;
-		this.zone.right .width = `${this.coords.x2 = imgW - x1 - i }px`;
-	}
-	get h ( ) { return this.coords.h }
-	set h (i) {
-		const { y1, imgH }      =    this.coords;
-		this.zone.select.height = `${this.coords.h  = (i = imgH < i + y1 ? imgH - y1 : Math.max(i,0)) }px`;
-		this.zone.bottom.height = `${this.coords.y2 = imgH - y1 - i }px`;
-	}
-	get rw ( ) { return this.coords.w }
-	set rw (i) { // "rw" it mean reverse width (expands the select area in the opposite direction)
-		const { x2, imgW }     =    this.coords;
-		this.zone.select.width = `${this.coords.w  = (i = imgW < i + x2 ? imgW - x2 : Math.max(i,0)) }px`;
-		this.zone.left  .width = `${this.coords.x1 = imgW - x2 - i }px`;
-	}
-	get rh ( ) { return this.coords.h }
-	set rh (i) {
-		const { y2, imgH }      =    this.coords;
-		this.zone.select.height = `${this.coords.h  = (i = imgH < i + y2 ? imgH - y2 : Math.max(i,0)) }px`;
-		this.zone.top   .height = `${this.coords.y1 = imgH - y2 - i }px`;
-	}
+
 	// get real coordinates of selected area
 	getCoords() { 
-		const { x1, y1, w, h, imgW, imgH, realW, realH } = this.coords,
+		const { x1, y1, w, h, imgW, imgH, realW, realH } = this._coords,
 			wR = realW / imgW,
 			hR = realH / imgH;
 		return [
 			Math.floor(x1 * wR), Math.floor(y1 * hR), // returns real scale [X, Y, W, H] of selection
 			Math.floor(w  * wR), Math.floor(h  * hR)  // it can be used in canvas for crop/cut image parts
 		]
-	}
-	// this is a general function for apply selection area on the image
-	selectZone(img, x = 0, y = 0, w = 50, h = 50) { // x, y, w, h  arguments is optional and set the start selection position
-		const imgW = img.width; // it just gets the real and actual width / height of the first argument passed and blindly bounces off of them.
-		const imgH = img.height; // you can call this function as much as you like for recalc area params
-		this.coords.realW = img.naturalWidth  || imgW; // if naturalWidth/naturalHeight data not contains in element
-		this.coords.realH = img.naturalHeight || imgH; // the width/height are counted as them
-
-		this.box.style.width  = `${this.coords.imgW = imgW }px`;
-		this.box.style.height = `${this.coords.imgH = imgH }px`;
-
-		//first set the X position of select zone and its width
-		this.zone.select.width = `${this.coords.w  = imgW < w + x ? imgW - x : w }px`;
-		this.zone.left  .width = `${this.coords.x1 = imgW < x + w ? imgW - w : x }px`;
-		this.zone.right .width = `${this.coords.x2 = imgW - x - w }px`;
-
-		//second set the Y position of select zone and its height
-		this.zone.select.height = `${this.coords.h  = imgH < h + y ? imgH - y : h }px`;
-		this.zone.top   .height = `${this.coords.y1 = imgH < y + h ? imgH - h : y }px`;
-		this.zone.bottom.height = `${this.coords.y2 = imgH - y - h }px`;
-
-		if (this.autoadd) { // note: just set this flag to false if you first argument passing is not an element
-			img.before(this.box); // or you can just doing something like:
-		} // { width: 500, ..., before: (box) => my_place.append(box) }
 	}
 
 	handleEvent(e) { // mouse/touch move handler
@@ -140,70 +143,75 @@ class pasL {
 		e.stopPropagation();
 		e.preventDefault();
 
-		const [name,type] = e.target.id.split('_');
+		const [name,type] = e.target.classList;
 		const startX      = point.clientX;
 		const startY      = point.clientY;
 		const lock        = this.lock;
 
-		const endX = this.coords[name === 'rcon' ? 'w' : 'x1'];
-		const endY = this.coords[name === 'rcon' ? 'h' : 'y1'];
+		const endX = this._coords[name === 'pasl-rcons' ? 'w' : 'x1'];
+		const endY = this._coords[name === 'pasl-rcons' ? 'h' : 'y1'];
 
 		const moveFunc = (
 			type === 't-l' || type === 'l-c' && lock ? ({ clientX, clientY }) => {
 			const rx = startX - clientX + endX;
 			const ry = startY - clientY + endY;
-			this. rw = lock && ry > rx ? ry : rx;
-			this. rh = lock && rx > ry ? rx : ry;
+			this.setWidth(lock && ry > rx ? ry : rx, true);
+			this.setHeight(lock && rx > ry ? rx : ry, true);
+			this.box.dispatchEvent(new CustomEvent('PasL sizeChange'));
 		} : type === 't-r' || type === 't-c' && lock ? ({ clientX, clientY }) => {
 			const x  = clientX - startX + endX;
 			const ry = startY - clientY + endY;
-			this. w  = lock && ry > x ? ry : x;
-			this. rh = lock && x > ry ? x : ry;
+			this.setWidth(lock && ry > x ? ry : x);
+			this.setHeight(lock && x > ry ? x : ry, true);
+			this.box.dispatchEvent(new CustomEvent('PasL sizeChange'));
 		} : type === 'b-l' || type === 'b-c' && lock ? ({ clientX, clientY }) => {
 			const y  = clientY - startY + endY;
 			const rx = startX - clientX + endX;
-			this. rw = lock && y > rx ? y : rx;
-			this. h  = lock && rx > y ? rx : y;
+			this.setWidth(lock && y > rx ? y : rx, true);
+			this.setHeight(lock && rx > y ? rx : y);
+			this.box.dispatchEvent(new CustomEvent('PasL sizeChange'));
 		} : type === 'b-r' || type === 'r-c' && lock ? ({ clientX, clientY }) => {
 			const x  = clientX - startX + endX;
 			const y  = clientY - startY + endY;
-			this. w  = lock && y > x ? y : x;
-			this. h  = lock && x > y ? x : y;
+			this.setWidth(lock && y > x ? y : x);
+			this.setHeight(lock && x > y ? x : y);
+			this.box.dispatchEvent(new CustomEvent('PasL sizeChange'));
 		} : type === 'l-c' ? ({ clientX }) => {
-			this. rw = startX - clientX + endX;
+			this.setWidth(startX - clientX + endX, true);
+			this.box.dispatchEvent(new CustomEvent('PasL sizeChange'));
 		} : type === 't-c' ? ({ clientY }) => {
-			this. rh = startY - clientY + endY;
+			this.setHeight(startY - clientY + endY, true);
+			this.box.dispatchEvent(new CustomEvent('PasL sizeChange'));
 		} : type === 'r-c' ? ({ clientX }) => {
-			this. w  = clientX - startX + endX;
+			this.setWidth(clientX - startX + endX);
+			this.box.dispatchEvent(new CustomEvent('PasL sizeChange'));
 		} : type === 'b-c' ? ({ clientY }) => {
-			this. h  = clientY - startY + endY;
+			this.setHeight(clientY - startY + endY);
+			this.box.dispatchEvent(new CustomEvent('PasL sizeChange'));
 		} :/* type = 'm-v' */({ clientX, clientY }) => {
-			this. x  = clientX - startX + endX;
-			this. y  = clientY - startY + endY;
+			this.setPosition(clientX - startX + endX, clientY - startY + endY);
+			this.box.dispatchEvent(new CustomEvent('PasL posChange'));
 		});
 		const attach   = mv => {
-			this.onMoveChange.forEach(callback => callback());
 			moveFunc(_PointHandler.getPos(mv));
 		}
 		const deattach = () => {
-			this.onMoveEnd.forEach(callback => callback());
+			this.box.dispatchEvent(new CustomEvent('PasL endAction'));
 			window.removeEventListener(_PointHandler.MOVE, attach);
 			window.removeEventListener(_PointHandler.END, deattach);
 		}
-		this.onMoveStart.forEach(callback => callback());
+		this.box.dispatchEvent(new CustomEvent('PasL startAction'));
 		window.addEventListener(_PointHandler.MOVE, attach);
 		window.addEventListener(_PointHandler.END, deattach);
 	}
 
 	addListener(name, callback) {
-		let list = this[`on${name}`];
-		if (list && typeof callback == 'function' && !list.includes(callback))
-			list.push(callback);
+		if (typeof callback === 'function' && ['startAction', 'posChange', 'sizeChange', 'endAction'].includes(name))
+			this.box.addEventListener(`PasL ${name}`, callback);
 	}
 	removeListener(name, callback) {
-		let list = this[`on${name}`];
-		if (list)
-			list.splice(1, list.indexOf(callback));
+		if (typeof callback === 'function' && ['startAction', 'posChange', 'sizeChange', 'endAction'].includes(name))
+			this.box.removeEventListener(`PasL ${name}`, callback);
 	}
 };
 
