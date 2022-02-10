c612e62424d5e66025246ad5a1c0a5b831904457
OpenA <adminother@gmail.com>
2022-02-10 13:13:28 +0500

    towL: scale, select, move objects

--- towL.js
+++ towL.js
@@ -17,14 +17,16 @@ class TowL extends PointTracker {
 		Object.defineProperties(this, {
 			/* public */
 			box : { enumerable: true, value: _Box },
-			svg : { enumerable: true, value: _Svg }
+			svg : { enumerable: true, value: _Svg },
+			_ratio: { writable: true, value: 1 }
 		});
-
 	}
-	setViewBox(w, h) {
-		this.svg.setAttribute('width' , w)
-		this.svg.setAttribute('height', h)
-		this.svg.setAttribute('viewBox', `0 0 ${w} ${h}`);
+
+	setViewBox(w = 100, h = 100, scale = 1) {
+		this.svg.setAttribute('width' , w);
+		this.svg.setAttribute('height', h);
+		this.svg.setAttribute('viewBox', `0 0 ${w / scale} ${h / scale}`);
+		this._ratio = 1 / scale;
 	}
 
 	toSource(blob = false) {
@@ -105,24 +107,51 @@ class TowL extends PointTracker {
 
 	_emitActive(points, data) {
 
-		const { svg } = this;
-		const { el }  = points[0];
-		const { selected } = svg.children;
-		let type = el.nodeName;
+		const { svg, box, _ratio } = this;
 
-		data.vboxW = svg.viewBox.baseVal.width;
-		data.vboxH = svg.viewBox.baseVal.height;
+		data.ratio = _ratio;
 
-		if (selected)
-			selected.removeAttribute('id');
-		if (type in TowL) {
-			data[0] = TowL[type](el);
-			svg.appendChild(el).id = 'selected';
+		const b0 = box.getBoundingClientRect(), sels = [];
+
+		for (let i = 0; i < points.length; i++) {
+
+			let { el } = points[i];
+
+			if (el === svg || el === box)
+				continue;
+
+			let { left, top, width, height } = el.getBoundingClientRect(),
+			    type = el.nodeName, el_class = el.classList, sel;
+
+			left -= b0.left, top -= b0.top;
+
+			if (type in TowL) {
+				sel = TowL.makeSelect(type,
+					(left -= 5), (top -= 5), (width += 10), (height += 10)
+				);
+				sel._figure = el;
+			} else if (el_class[0] === 'towL-select') {
+				type = el_class[1].substring('towL-'.length);
+				el = (sel = el)._figure;
+			}
+			data[i] = TowL[type](el);
+			data[i].sel = sel;
+			data[i].start_x = left;
+			data[i].start_y = top;
+			data[i].start_w = width;
+			data[i].start_h = height;
+			svg.append(el);
+			sels.push( box.appendChild(sel) );
+		}
+		for (const sel of box.getElementsByClassName('towL-select')) {
+			sel.classList[ sels.includes(sel) ? 'add' : 'remove' ]('act-v');
 		}
 	}
 	_emitChange(points, data) {
 		if (data[0]) {
-			data[0].move(points[0].x, points[0].y);
+			data[0].move(points[0].x * data.ratio, points[0].y * data.ratio);
+			data[0].sel.style.left = (data[0].start_x + points[0].x) +'px';
+			data[0].sel.style.top  = (data[0].start_y + points[0].y) +'px';
 		}
 	}
 }
@@ -166,4 +195,25 @@ TowL.rect = (el) => {
 			el.y.baseVal.value = sY + y;
 		}
 	}
+}
+
+TowL.makeSelect = (type, l, t, w, h) => {
+	const sel = _setup('div', { class: 'towL-select towL-'+ type, 
+		style: `position: absolute; left: ${l}px; top: ${t}px; width: ${w}px; height: ${h}px;`
+	});
+	if (type === 'text') {
+	
+	} else if (type === 'line') {
+		sel.append(
+			_setup('div', { class: 'towL-point s-d' }),
+			_setup('div', { class: 'towL-point e-d' })
+		);
+	} else
+		sel.append(
+			_setup('div', { class: 'pasL-rcons l-t' }),
+			_setup('div', { class: 'pasL-rcons r-t' }),
+			_setup('div', { class: 'pasL-rcons l-b' }),
+			_setup('div', { class: 'pasL-rcons r-b' })
+		);
+	return sel;
 }
